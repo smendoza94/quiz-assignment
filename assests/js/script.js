5cbc27310d16f053f5af87fdb0647f72054d511d
StevenM <stevenbmendoza@gmail.com>
2022-02-20 13:54:44 -0800

    added js high score button

---
+++ assests/js/script.js
@@ -0,0 +1,7 @@
+var body = document.body;
+
+var highScoreBtn = document.createElement(`button`);
+highScoreBtn.textContent = "High Scores";
+highScoreBtn.setAttribute(`style`, `border: 1px; background: none; color: purple;`)
+body.appendChild(highScoreBtn);
+
