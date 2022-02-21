b3354806e2f0cc588d17537867bf6a169b34a518
StevenM <stevenbmendoza@gmail.com>
2022-02-21 10:37:40 -0800

    header high score button styled, js score countdown timer

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,24 @@
-var body = document.body;
+var timerEl = document.getElementById('timer');
+// Timer that counts down from 100
+function countdown() {
+  var timeLeft = 100;
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 0) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = 'Score: ' + timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = 'Score: 0';
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      displayMessage();
+    }
+  }, 1000);
+}
 
-var highScoreBtn = document.createElement(`button`);
-highScoreBtn.textContent = "High Scores";
-highScoreBtn.setAttribute(`style`, `border: 1px; background: none; color: purple;`)
-body.appendChild(highScoreBtn);
-
+countdown();
